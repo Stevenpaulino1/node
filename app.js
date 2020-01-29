f8571e86489235a12f522c1e608c7937559d352d
Steven Paulino <stevenpaulino1@hotmail.com>
2020-01-29 12:27:33 -0500

    added public/css static files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:false}))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/admin",routesAdmin)
 app.use(routesShop)
