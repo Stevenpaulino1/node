4f30d0970570324e5f1dab23e69b9630c63da59a
Steven Paulino <stevenpaulino1@hotmail.com>
2020-02-03 17:46:21 -0500

    storing data in file PRE DB

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,24 @@
-const products = []
+const Product = require('../models/product')
 
 exports.getAddProduct = (req,res,next)=>{
     res.render('add-product.ejs', {pageTitle:"Add Product", path:"admin/add-product"})
 }
 
 exports.postAddProduct = (req,res,next)=>{
-    products.push({title: req.body.title})
-    console.log('PRODUCT',products);
+    const product = new Product(req.body.title)
+    product.save()
     console.log("REDIRECTING");
     res.redirect('/')
 }
 
 exports.getProducts = (req,res,next)=>{
-    res.render('shop',{
-        prods: products,
-        pageTitle:"Shop", 
-        path: '/',
-        hasProducts: products.length>0
-    })
+  Product.fetchAll((products)=>{
+      res.render('shop',{
+          prods: products,
+          pageTitle:"Shop", 
+          path: '/',
+          hasProducts: products.length>0
+      })
+  })
     // can omit pug bc we already told app.js pug is the templating engine
 }
