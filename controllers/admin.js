af3d476109870f56addeb98cbdb504ac8c7b5229
Steven Paulino <stevenpaulino1@hotmail.com>
2020-04-06 16:55:39 -0400

    delete prod action

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,6 +84,6 @@ exports.getProducts = (req,res,next)=>{
 
 exports.postDeleteProduct = (req,res,next)=>{
     const prodId = req.body.productId
-    Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
     res.redirect('/admin/products')
 }
