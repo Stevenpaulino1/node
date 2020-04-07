affc1929f0fcd1fdf51d62ed6ac175f483293e09
Steven Paulino <stevenpaulino1@hotmail.com>
2020-04-07 15:37:45 -0400

    adding user model

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ app.set("views", "views")
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req,res,next)=>{
-//     User.findById("5e8b6bfc57683f02046fb4d6")
-//     .then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id)
-//         next()
-//     })
-//     .catch(err => console.log("APP ERROR",err))
-// })
+app.use((req,res,next)=>{
+    User.findById("5e8cd4f12548b3531898feed")
+    .then(user => {
+        req.user = user
+        next()
+    })
+    .catch(err => console.log("APP ERROR",err))
+})
 
 
 app.use("/admin",admin.routes)
@@ -42,6 +42,17 @@ app.use(errorController.get404)
 
 mongoose.connect(`mongodb+srv://stevenpaulino1:${password}@cluster0-13cav.mongodb.net/shop?retryWrites=true&w=majority
 `).then(result=>{
+    User.findOne().then(user =>{
+        if (!user){
+            const user = new User({
+                name: "Steven Paulino",
+                email: "test@gmail.com",
+                cart: { items: []}
+            })
+
+            user.save()
+        }
+    })
     app.listen('8003')
 }).catch(err => console.log(err))
 
